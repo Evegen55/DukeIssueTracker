9284426204fec3825737deb95a08c0b8a3df862e
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-04-13 17:56:40 +0300

    Initial commit

---
+++ src/main/java/org/mvc/dukeissuetrackermvc/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package org.mvc.dukeissuetrackermvc;
+
+/**
+ * @author (created on 4/13/2017).
+ */
+import java.util.HashMap;
+import java.util.Map;
+import javax.mvc.security.Csrf;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+// Optionally this configuration can occur within
+// the web.xml deployment descriptor
+@ApplicationPath("tracker")
+public class ApplicationConfig extends Application {
+// Additional configuration code, if needed
+}
